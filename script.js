6e806bcf38c473f1afb018b2c95087d1a4864c8d
Oäk Kingkaew <oakabc@gmail.com>
2024-05-09 16:06:01 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let count = 0; 
+let count = 0; // เอาไว้นับคะแนนที่กดแล้วถูกต้อง
 
-function checkAnswer1(answer) {
-    const result = document.getElementById('result1');
+function checkAnswer(answer) {
+    const result = document.getElementById('result');
     if (answer === 'a') {
         result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
         result.style.color = 'green';
-        count++;
+        count = 1; // count = count + 1;
     } else {
         result.textContent = "Incorrect! Try again.";
         result.style.color = 'red';
@@ -16,13 +16,15 @@ function checkAnswer2(answer) {
     const result = document.getElementById('result2');
     const result3 = document.getElementById('result3');
     if (answer === 'd') {
-        result.textContent = "ถูกต้อง PIM ก่อตั้งขึ้นเมื่อปี 2550";
+        result.textContent = "ถูกต้อง PIM ก่อตั้งเมื่อปี 2550";
         result.style.color = 'green';
-        count++;
+        count++; // บวกคะแนนเพิ่ม 1 คะแนน ถ้ากดถูก
     } else {
         result.textContent = "Incorrect! Try again.";
         result.style.color = 'red';
     }
-    result3.textContent = "คุณได้ทั้งหมด " + count + " คะแนน";
+
+    result3.textContent = "คุณได้คะแนนทั้งหมด " + count + " คะแนน"
     count = 0;
 }
+ 
